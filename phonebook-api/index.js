387afc5b00487430fb4fcfcb7a4b5a825d4fb6ca
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-02-08 17:22:00 +0200

    Excercise: 3.8*: puhelinluettelon backend step8

--- phonebook-api/index.js
+++ phonebook-api/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 app.use(express.json());
+
+morgan.token('body', (req) => { 
+  return req.method === 'POST' ? 
+  JSON.stringify(req.body) 
+  : ''; 
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(morgan('tiny'));
 
 let persons = [
