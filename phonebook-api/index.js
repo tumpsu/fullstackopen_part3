30cd07e4effd558d0a581f66bc856b90917e2691
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-02-09 20:24:50 +0200

    Modified banckend to support Render publish.

--- phonebook-api/index.js
+++ phonebook-api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+app.use(express.static('dist'));
+app.use(cors());
 app.use(express.json());
 
 morgan.token('body', (req) => { 
@@ -116,7 +119,7 @@ app.post('/api/persons', (req, res) => {
     .json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
